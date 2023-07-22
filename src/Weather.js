16393e54feee8171db1dae71051338c83da5ef50
itsdianaa21 <dianamenendez17@gmail.com>
2023-07-22 17:27:41 -0500

    created base for react app

---
+++ src/Weather.js
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}°`);
+  }
+  let apiKey = "3f6be1c407b0d9d1933561808db358ba";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <h2>Hello from Weather</h2>;
+}
